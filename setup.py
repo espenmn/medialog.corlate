bd7a706b024ede01cd0ed27193ca51621b93591f
Espen Moe-Nilssen <espen@medialog.no>
2015-03-26 16:10:16 +0100

    Bootstrap theme for Plone 4

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='medialog.corlate',
+      version=version,
+      description="Corlate theme for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Medialog theme Plone bootstrap',
+      author='Espen Moe-Nilssen',
+      author_email='espen@medialog.no',
+      url='http://github.com/espenmn/medialog.corrolate',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'z3c.jbot',
+          'diazotheme.frameworks',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
